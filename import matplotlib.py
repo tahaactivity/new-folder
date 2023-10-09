7178647527c045676fde069ba1791009caee15ab
tahaactivity <125874922+tahaactivity@users.noreply.github.com>
2023-10-09 20:14:24 +0500

    matplotlib file

---
+++ import matplotlib.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Sample data
+x = [1, 2, 3, 6, 7,]
+y = [10, 12, 5, 8, 7]
+
+# Create a basic line plot
+plt.plot(x, y)
+
+# Add labels and a title
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('Simple Matplotlib Plot')
+
+# Display the plot
+plt.show()
